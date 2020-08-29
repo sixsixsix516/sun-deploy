dc8a8cb7fc79fbcac90ea856a877244b36fde96c
sun <sixsixsix516@163.com>
2020-08-29 21:35:53 +0800

    完善

--- src/main/java/com/sixsixsix516/SftpUtil.java
+++ src/main/java/com/sixsixsix516/SftpUtil.java
@@ -1,11 +1,24 @@
+/*
+ * Copyright (c) 2011-2020, sun (sixsixsix516@163.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package com.sixsixsix516;
 
 import com.jcraft.jsch.*;
-import org.apache.commons.io.IOUtils;
-
 import java.io.*;
 import java.util.Properties;
-import java.util.Vector;
 
 
 public class SftpUtil {
--- src/main/java/com/sixsixsix516/ssh/MyUserInfo.java
+++ src/main/java/com/sixsixsix516/ssh/MyUserInfo.java
@@ -1,12 +1,23 @@
-package com.sixsixsix516.ssh;
-
-/**
- * @author sun 2020/8/28 17:15
+/*
+ * Copyright (c) 2011-2020, sun (sixsixsix516@163.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 
+
+package com.sixsixsix516.ssh;
+
 import com.jcraft.jsch.UserInfo;
-
-
 public class MyUserInfo implements UserInfo  {
 
     private String password;
@@ -18,7 +29,7 @@ public class MyUserInfo implements UserInfo  {
 
     @Override
     public String getPassword() {
-        return "server2008!@";
+        return password;
     }
 
     @Override
